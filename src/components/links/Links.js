85f3148e1aa19dea9d92c686c5e6b93568eb96c5
Olaniran Emmanuel <114101461+NueltCodes@users.noreply.github.com>
2022-10-30 00:45:37 +0300

    Update Links.js

--- src/components/links/Links.js
+++ src/components/links/Links.js
@@ -7,34 +7,46 @@ const Links = () => {
       <div className="linksContainer">
         <div className="link frame">
           <div className="text">
-            <a href="https://twitter.com/NueltCodes/">Twitter Link</a>
+            <a href="https://twitter.com/NueltCodes/"target="_blank"
+              rel="noopener noreferrer"
+            >Twitter Link</a>
           </div>
         </div>
         <div className="link frame">
           <div className="text">
-            <a href="https://training.zuri.team/">Zuri Team</a>
+            <a href="https://training.zuri.team/target="_blank"
+              rel="noopener noreferrer"
+            >Zuri Team</a>
           </div>
         </div>
         <div className="link frame">
           <div className="text">
-            <a href="https://books.zuri.team/">Zuri Books</a>
+            <a href="https://books.zuri.team/target="_blank"
+              rel="noopener noreferrer"
+            >Zuri Books</a>
           </div>
         </div>
         <div className="link frame">
           <div className="text">
-            <a href="twitter">Python Books</a>
+            <a href="https://books.zuri.team/"target="_blank"
+              rel="noopener noreferrer"
+            >Python Books</a>
           </div>
         </div>
         <div className="link frame">
           <div className="text">
-            <a href="https://background.zuri.team/">
+            <a href="https://background.zuri.team/"target="_blank"
+              rel="noopener noreferrer"
+            >
               Background Check for Coders
             </a>
           </div>
         </div>
         <div className="link frame">
           <div className="text">
-            <a href="https://books.zuri.team/design-rules">Design Books</a>
+            <a href="https://books.zuri.team/design-rules"target="_blank"
+              rel="noopener noreferrer"
+            >Design Books</a>
           </div>
         </div>
       </div>
